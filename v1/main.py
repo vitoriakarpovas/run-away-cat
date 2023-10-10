6910dd45b4df438dd80271280cd4b829d2052b8e
Gustavo E. Cruz <64294820+Cr00zz@users.noreply.github.com>
2023-10-10 18:21:53 -0300

    Update main.py
    MODIFICAÇÕES:
    
    - Implementada a física do gato pulando
    - Implementado (por enquanto) a modificação do tamanho do sprite do gato para testes

--- v1/main.py
+++ v1/main.py
@@ -1,106 +1,60 @@
-#----------------PARTE 1-------------------------------------
-
-# Importa as bibliotecas utilizadas
 import sys, pygame
 from pygame.locals import *
-from random import *
 
-# Inicializa a biblioteca pygame
 pygame.init()
 
-# Cria a surface 
-size = (800, 600)
+size = (626, 626)
 screen = pygame.display.set_mode(size)
-
-# Define um titulo para a janela
 pygame.display.set_caption("Jogo do Gatinho")
 
-#Carrega a imagem de fundo
-imagem = pygame.image.load("ceu_azul.png")
 gato = pygame.image.load("gato.png")
+imagem = pygame.image.load("ceu_azul.png")
 
-# Define as cores em RGB
-BLACK  = (0, 0, 0)
+BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 YELLOW = (255, 255, 0)
 RED = (255, 0, 0)
 
-# Declarando a fonte do placar e variável contadora
-font = pygame.font.SysFont('sans',40)
+font = pygame.font.SysFont('sans', 40)
 placar = 0
-
-# Declara a posicao X e Y do gato 
-posicaoGato = [400, 100]
-
-# Armazena num vetor a Velocidade de movimentacao do circulo 
-velocidadeGato = 5
-
-# Variáveis de posição do gato
-X_gato = 0
-Y_gato = 0
-
-# Variável para contagem de tempo, utilizado para controlar a velocidade de quadros (de atualizações da tela)
 clock = pygame.time.Clock()
+CLOCKTICK = pygame.USEREVENT + 1
+pygame.time.set_timer(CLOCKTICK, 1000)
 
-#criando objeto Clock
-CLOCKTICK = pygame.USEREVENT+1
-pygame.time.set_timer(CLOCKTICK, 1000) # configurado o timer do Pygame para execução a cada 1 segundo
+class Gato:
+    def __init__(self):
+        self.image = pygame.transform.scale(gato, (100, 100))
+        self.rect = self.image.get_rect()
+        self.rect.center = (150, 330)
+        self.velocidade_y = 0
 
+    def update(self):
+        self.velocidade_y += 1  # Simula a gravidade
+        self.rect.y += self.velocidade_y
 
-#----------------PARTE 2-------------------------------------
+        if self.rect.bottom > 626:  # Quando toca na parte de baixo da tela
+            self.rect.bottom = 626
+            self.velocidade_y = -20  # "Pular" para cima
 
-# Loop principal do jogo
+gato = Gato()
+
 while True:
-    
-    # Verifica se algum evento aconteceu
     for event in pygame.event.get():
-        # Verifica se foi um evento de saida (pygame.QUIT), 
-        # em caso afirmativo fecha a aplicacao
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-    # Verifica se alguma tecla foi pressionada, e captura o evento
     pressed = pygame.key.get_pressed()
+    if pressed[pygame.K_LEFT]:
+        gato.rect.x -= 5
+    if pressed[pygame.K_RIGHT]:
+        gato.rect.x += 5
 
-    #Verifica qual tecla (seta) foi pressionada e atualiza o vetor Posicao de acordo com a Velocidade
-    if pressed[pygame.K_LEFT]: posicaoGato[0] -= velocidadeGato
-    if pressed[pygame.K_RIGHT]: posicaoGato[0] += velocidadeGato
-
-    #blita a imagem de fundo na tela
     screen.blit(imagem, (0, 0))
+    screen.blit(gato.image, gato.rect)
+    gato.update()
 
-    # Desenha o gato na tela
-    screen.blit(gato, posicaoGato)
-
-    # Velocidade de queda do círculo Vermelho
-    Y_gato -= 5 
-
-    # renderizando as fontes do placar na tela
-    score1 = font.render('Placar '+str(placar), True, (WHITE))
+    score1 = font.render('Placar ' + str(placar), True, WHITE)
     screen.blit(score1, (600, 50))
-        
-    # Atualiza a tela visivel ao usuario
     pygame.display.flip()
-
-    # Limita a taxa de quadros (framerate) a 60 quadros por segundo (60fps)
     clock.tick(60)
-
-#final de jogo
-#Limpando a tela do jogo
-frame = pygame.draw.rect(screen, (WHITE), Rect((0, 0), (800, 600)))
-
-
-textofinal = font.render('Fim de Jogo - Placar final: ' + str(placar), True, (RED))
-size = font.size(str(textofinal))
-screen.blit(textofinal, (size[0]/2., size[1]/2.))
-
-
-#atualizamos a tela com uma nova tela de informação final ao jogador
-pygame.display.flip()
-#pequeno loop game esperando o usuario encerrar
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
